1a949d24ca42fd5a63047f330f461f7c7110e569
Billy Trend <billy@cohere.com>
2024-04-29 15:31:36 +0100

    Fix types

--- index.ts
+++ index.ts
@@ -1,15 +1,16 @@
 
-import { JsonSchema7LiteralType, JsonSchema7ObjectType, JsonSchema7TypeUnion } from "zod-to-json-schema";
+import { Cohere } from "cohere-ai";
+import { JsonSchema7LiteralType, JsonSchema7ObjectType } from "zod-to-json-schema";
 
 const nestDelimiter = "__";
 
-type ParameterDefinitions = Required<ParameterDefinitions>;
+type ParameterDefinitions = Required<Cohere.Tool["parameterDefinitions"]>;
 
-export const convert = (schema: JsonSchema7TypeUnion): ParameterDefinitions => {
+export const convert = (schema: JsonSchema7ObjectType): ParameterDefinitions => {
     return flattenObject(schema)
 }
 
-export const unconvert = (tool: ParameterDefinitions): JsonSchema7ObjectType => {
+export const unconvert = (tool: object): object => {
     return unflattenObject(tool);
 }
 
@@ -17,12 +18,17 @@ const flattenObject = (schema: JsonSchema7ObjectType, prefix: string = ""): Para
     let result: ParameterDefinitions = {};
     for (const [key, value] of Object.entries(schema.properties)) {
         const path = [prefix, key].filter(Boolean).join(nestDelimiter)
+
+        if (!("type" in value)) {
+            throw new Error("Type is missing from schema");
+        }
+
         if (value.type === "object") {
-            result = { ...result, ...flattenObject(value, path) };
+            result = { ...result, ...flattenObject(value as JsonSchema7ObjectType, path) };
         } else {
             result[path] = {
                 description: value.description,
-                type: convertToPythonType(value.type),
+                type: convertToPythonType(value.type as JsonSchema7LiteralType["type"]),
                 required: schema.required?.includes(key) ?? false
             }
         }
@@ -31,19 +37,17 @@ const flattenObject = (schema: JsonSchema7ObjectType, prefix: string = ""): Para
 }
 
 const unflattenObject = (params: object): object => {
-    let result: JsonSchema7ObjectType = {
-    };
+    let result: Record<string, unknown> = {};
 
     for (const [key, value] of Object.entries(params)) {
         const path = key.split(nestDelimiter);
-        let current = result;
+        let current: Record<string, unknown> = result;
         for (let i = 0; i < path.length - 1; i++) {
             if (current[path[i]] === undefined) {
-                current[path[i]] = {
-                }
+                current[path[i]] = {}
             }
 
-            current = current[path[i]];
+            current = current[path[i]] as Record<string, unknown>;
         }
         current[path[path.length - 1]] = value
     }
